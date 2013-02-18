fab25d1ed0710e2fc61de525f40f43077f04c486
cioc <cclego@gmail.com>
2013-02-18 00:32:17 +0000

    updated test

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 from Checkpoint import Checkpoint
 
-ch = Checkpoint(bucket='ml-charles')
-
-print ch.list()
+ch = Checkpoint(bucket='bucketnamehere')
 
 x = [1,2,3,4,5,6, 7]
 
@@ -11,3 +9,9 @@ ch.store('mylist1', x)
 y = ch.load('mylist1')
 
 print y
+
+l = ch.list()
+for i in l:
+  print i.key
+
+
